1bc8fcb9a1c591d2a68a706fd172374a7cc57d3e
BalabanJoshua <balabaniosua@gmail.com>
2021-09-16 18:57:38 +0300

    Hello Github

---
+++ probleme/urare_941.cpp
@@ -0,0 +1,19 @@
+// ID: 941
+// Nume Problema: Urare
+// Credit: Me (daca doriti)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "Hello Github!" << endl; // Afiseaza Hello Github
+    return 0;
+}
+
+/*
+
+NOTE:
+Folositi comentarii ca sa explicati ce face codul (nu trebuie chiar la fiecare linie de cod c:, use common sense)
+
+*/
